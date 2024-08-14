32f699f384be8de3300cab3dae5cc82af2f7cd9d
KISUSU <119473141+kisusu115@users.noreply.github.com>
2024-08-14 15:08:20 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 52 ms, Memory: 10440 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+// 그래프를 만들고, 1에서 bfs나 dfs를 했을때 이전 노드이면 찾는거네
+using namespace std;
+
+int N;
+vector<int> map[100002];
+int visited[100002];
+int parent[100002];
+
+void dfs(int n) {
+	visited[n] = 1;
+	for (int i : map[n]) {
+		if (visited[i] != 1) {
+			parent[i] = n;
+			dfs(i);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N-1; i++) {
+		int u, v; cin >> u >> v;
+		map[u].push_back(v);
+		map[v].push_back(u);
+	}
+
+	dfs(1);
+
+	for (int i = 2; i <= N; i++) {
+		cout << parent[i] << "\n";
+	}
+	return 0;
+}
