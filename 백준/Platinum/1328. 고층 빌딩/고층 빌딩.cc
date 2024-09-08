f5d69159694afc55fee25949c9e6f172a15e800e
KISUSU <119473141+kisusu115@users.noreply.github.com>
2024-09-08 22:51:29 +0900

    [Platinum V] Title: 고층 빌딩, Time: 664 ms, Memory: 10148 KB -BaekjoonHub

--- 백준/Platinum/1328. 고층 빌딩/고층 빌딩.cc
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.cc
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string.h>
 
+#define DIVIDE 1000000007
+
 using namespace std;
 unsigned long long DP[101][101][101];
 unsigned long long C[101][101];
@@ -10,7 +12,7 @@ unsigned long long combination(int a, int b) {
 	unsigned long long ret;
 	if (a == b || b == 0) ret = 1;
 	else ret = combination(a - 1, b - 1) + combination(a - 1, b);
-	ret %= 1000000007;
+	ret %= DIVIDE;
 	C[a][b] = ret;
 	return ret;
 }
@@ -24,13 +26,13 @@ unsigned long long GS(int n, int l, int r) {
 	if (l == 1) {
 		for (int i = 1; i <= n - r + 1; i++) {
 			sum += GS(n - 1, i, r - 1);
-			sum %= 1000000007;
+			sum %= DIVIDE;
 		}
 	}
 	else if (r == 1) {
 		for (int i = 1; i <= n - l + 1; i++) {
 			sum += GS(n - 1, l - 1, i);
-			sum %= 1000000007;
+			sum %= DIVIDE;
 		}
 	}
 	else {
@@ -38,17 +40,17 @@ unsigned long long GS(int n, int l, int r) {
 			unsigned long long Lsum = 0; unsigned long long Rsum = 0;
 			for (int j = 1; j <= i - l + 2; j++) {
 				Lsum += GS(i, l - 1, j);
-				Lsum %= 1000000007;
+				Lsum %= DIVIDE;
 			}
 			for (int j = 1; j <= n - i - r + 1; j++) {
 				Rsum += GS(n - 1 - i, j, r - 1);
-				Rsum %= 1000000007;
+				Rsum %= DIVIDE;
 			}
-			sum += (((Lsum % 1000000007) * (Rsum % 1000000007)) % 1000000007) * (combination(n - 1, i) % 1000000007) % 1000000007;
-			sum %= 1000000007;
+			sum += (((Lsum % DIVIDE) * (Rsum % DIVIDE)) % DIVIDE) * (combination(n - 1, i) % DIVIDE) % DIVIDE;
+			sum %= DIVIDE;
 		}
 	}
-	DP[n][l][r] = sum %= 1000000007;
+	DP[n][l][r] = sum %= DIVIDE;
 	return DP[n][l][r];
 }
 
