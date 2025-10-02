302be99bd13fd5a1f5b01e999ec68fd3923946e3
KISUSU <119473141+kisusu115@users.noreply.github.com>
2025-10-02 13:53:28 +0900

    [level 0] Title: 문자열 출력하기, Time: 133.89 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Solution {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    public static void main(String[] args) throws Exception {
+        String str = br.readLine();
+        bw.write(str);
+        bw.flush();
+    }
+}
